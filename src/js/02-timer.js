bc3b1a8da6745ea1035fdf80350c29cd7f4a3734
OleksiiPanaioti <cavedzgn@gmail.com>
2023-03-04 19:30:35 +0200

    2 завдання працює майже весь функціонал

---
+++ src/js/02-timer.js
@@ -0,0 +1,126 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+// Отримуємо елементи з DOM
+refs = {
+    
+}
+const datePicker = document.getElementById("datetime-picker");
+const startButton = document.querySelector("[data-start]");
+const daysField = document.querySelector("[data-days]");
+const hoursField = document.querySelector("[data-hours]");
+const minutesField = document.querySelector("[data-minutes]");
+const secondsField = document.querySelector("[data-seconds]");
+
+startButton.disabled = true;
+
+// Змінна для зберігання ID таймера
+let timerId;
+
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+    minuteIncrement: 1,
+  
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const now = new Date();
+    if (selectedDate <= now) {
+      window.alert("Please choose a date in the future");
+      startButton.disabled = true;
+    } else {
+      startButton.disabled = false;
+    }
+  },
+};
+
+
+
+// Ініціалізація flatpickr на елементі datePicker з опціями
+const fp = flatpickr(datePicker, options);
+
+
+
+
+const countdown = {
+    start() {
+        const startTime = fp.selectedDates[0].getTime();
+
+        setInterval(() => {
+            const endTime = new Date().getTime();
+            const deltaTime = startTime - endTime;
+
+            const timeComponents = convertMs(deltaTime);
+
+            const seconds = timeComponents.seconds;
+            const minutes = timeComponents.minutes;
+            const hours = timeComponents.hours;
+            const days = timeComponents.days
+
+            if (deltaTime <= 0) {
+                // Countdown has ended
+                daysField.textContent = "00";
+                hoursField.textContent = "00";
+                minutesField.textContent = "00";
+                secondsField.textContent = "00";
+            } else if (days > 0) {
+                // Display remaining time in days
+                daysField.textContent = days.toString();
+                hoursField.textContent = pad(hours);
+                minutesField.textContent = pad(minutes);
+                secondsField.textContent = pad(seconds);
+            } else {
+                // Display remaining time in hours, minutes, and seconds
+                hoursField.textContent = hours.toString();
+                minutesField.textContent = pad(minutes);
+                secondsField.textContent = pad(seconds);
+            }
+            
+
+            
+        }, 1000);
+
+    },
+};
+
+
+
+startButton.addEventListener('click', countdown.start);
+
+function pad(time) {
+
+    return time.toString().padStart(2, "0");
+}
+
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+
+
+
+
+
